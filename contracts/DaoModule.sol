ba7c919a7b70a1bbf7b9f982d5d34d411ec63ab7
Richard Meissner <msc.meissner@gmail.com>
2021-01-25 22:57:47 +0100

    Sample code

--- contracts/DaoModule.sol
+++ contracts/DaoModule.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity >=0.8.0;
 
+import "./interfaces/Realitio.sol";
+
 contract Enum {
     enum Operation {
         Call,
@@ -20,4 +22,57 @@ interface Executor {
 }
 
 contract DaoTxModule {
+
+    Executor public executor;
+    Realitio public oracle;
+    uint256 public template;
+    mapping(bytes32 => bool) executedPropsals;
+
+    constructor(Executor _executor, Realitio _oracle) {
+        executor = _executor;
+        oracle = _oracle;
+        // We assume the question is "Did proposal <proposalHash> decide to execute the Safe tx with hash <txHash>"
+        template = oracle.createTemplate('{"proposalHash": "%s", "txHash": "%s", "type": "bool"}');
+    }
+
+    function addProposal(bytes32 proposalHash, address to, uint256 value, bytes memory data, Enum.Operation operation) public {
+        uint256 templateId = template;
+        bytes32 txHash = getTransactionHash(to, value, data, operation);
+        // This is not 100% correct as it is not a string concat but oprates on bytes
+        string memory question = string(abi.encodePacked(proposalHash, bytes3(0xe2909f), txHash));
+        bytes32 expectedProposalId = getProposalId(
+            templateId, question, address(this), 24 * 3600, 0, 0
+        );
+        // timeout == 24h
+        bytes32 proposalId = oracle.askQuestion(templateId, question, address(this), 24 * 3600, 0, 0);
+        require(expectedProposalId == proposalId, "Unexpected proposal id");
+    }
+
+    function executeProposal(bytes32 proposalHash, address to, uint256 value, bytes memory data, Enum.Operation operation) public {
+        uint256 templateId = template;
+        bytes32 txHash = getTransactionHash(to, value, data, operation);
+        // This is not 100% correct as it is not a string concat but oprates on bytes
+        string memory question = string(abi.encodePacked(proposalHash, bytes3(0xe2909f), txHash));
+        bytes32 proposalId = getProposalId(
+            templateId, question, address(this), 24 * 3600, 0, 0
+        );
+        require(!executedPropsals[proposalId], "Cannot execute transaction again");
+        executedPropsals[proposalId] = true;
+        // We expect a boolean as an answer (1 == true)
+        require(oracle.resultFor(proposalId) == bytes32(uint256(1)), "Transaction was not approved");
+        require(oracle.getFinalizeTS(proposalId) + 24 hours > block.timestamp, "Wait for additional cooldown");
+        executor.execTransactionFromModule(to, value, data, operation);
+    }
+
+    function getProposalId(uint256 templateId, string memory question, address arbitrator, uint32 timeout, uint32 openingTs, uint256 nonce) public view returns(bytes32) {
+        bytes32 contentHash = keccak256(abi.encodePacked(templateId, openingTs, question));
+        return keccak256(abi.encodePacked(contentHash, arbitrator, timeout, this, nonce));
+    }
+
+    function getTransactionHash(address to, uint256 value, bytes memory data, Enum.Operation operation) public view returns(bytes32) {
+        // TODO: EIP-712
+        return keccak256(abi.encode(this, to, value, data, operation));
+    }
+
+    // TODO: add arbritration methods
 }
---
+++ contracts/interfaces/Realitio.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity >=0.8.0;
+
+interface Realitio {
+
+    // mapping(bytes32 => Question) public questions;
+
+    /// @notice Ask a new question without a bounty and return the ID
+    /// @dev Template data is only stored in the event logs, but its block number is kept in contract storage.
+    /// @dev Calling without the token param will only work if there is no arbitrator-set question fee.
+    /// @dev This has the same function signature as askQuestion() in the non-ERC20 version, which is optionally payable.
+    /// @param template_id The ID number of the template the question will use
+    /// @param question A string containing the parameters that will be passed into the template to make the question
+    /// @param arbitrator The arbitration contract that will have the final word on the answer if there is a dispute
+    /// @param timeout How long the contract should wait after the answer is changed before finalizing on that answer
+    /// @param opening_ts If set, the earliest time it should be possible to answer the question.
+    /// @param nonce A user-specified nonce used in the question ID. Change it to repeat a question.
+    /// @return The ID of the newly-created question, created deterministically.
+    function askQuestion(
+        uint256 template_id, string calldata question, address arbitrator, uint32 timeout, uint32 opening_ts, uint256 nonce
+    ) external returns (bytes32);
+
+    /// @notice Report whether the answer to the specified question is finalized
+    /// @param question_id The ID of the question
+    /// @return Return true if finalized
+    function isFinalized(bytes32 question_id) view external returns (bool);
+
+    /// @notice Return the final answer to the specified question, or revert if there isn't one
+    /// @param question_id The ID of the question
+    /// @return The answer formatted as a bytes32
+    function resultFor(bytes32 question_id) external view returns (bytes32);
+
+    /// @notice Returns the timestamp at which the question will be/was finalized
+    /// @param question_id The ID of the question 
+    function getFinalizeTS(bytes32 question_id) external view returns(uint32);
+
+    /// @notice Returns whether the question is pending arbitration
+    /// @param question_id The ID of the question 
+    function isPendingArbitration(bytes32 question_id) external view returns(bool);
+
+    /// @notice Create a reusable template, which should be a JSON document.
+    /// Placeholders should use gettext() syntax, eg %s.
+    /// @dev Template data is only stored in the event logs, but its block number is kept in contract storage.
+    /// @param content The template content
+    /// @return The ID of the newly-created template, which is created sequentially.
+    function createTemplate(string calldata content) external returns (uint256);
+}
