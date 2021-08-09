a6e50ccfd7bfdd60a60f2fd10b047746a3c2bc81
cbrzn <cesarbrazon10@gmail.com>
2021-08-10 00:04:06 +0200

    chore: documentation and scripts updated

--- src/tasks/setup.ts
+++ src/tasks/setup.ts
@@ -8,31 +8,33 @@ const FIRST_ADDRESS = "0x0000000000000000000000000000000000000001";
 const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 
 task("setup", "Provides the clearing price to an auction")
-    .addParam("dao", "Address of the DAO (e.g. Safe)", undefined, types.string)
+    .addParam("owner", "Address of the owner", undefined, types.string)
+    .addParam("executor", "Address of the executor (e.g. Safe)", undefined, types.string)
     .addParam("oracle", "Address of the oracle (e.g. Realitio)", undefined, types.string)
     .addParam(
         "template", 
         "Template that should be used for proposal questions (See https://github.com/realitio/realitio-dapp#structuring-and-fetching-information)", 
         undefined, 
         types.string
-    )
-    .addParam("timeout", "Timeout in seconds that should be required for the oracle", 48 * 3600, types.int, true)
-    .addParam("cooldown", "Cooldown in seconds that should be required after a oracle provided answer", 24 * 3600, types.int, true)
-    .addParam("expiration", "Time duration in seconds for which a positive answer is valid. After this time the answer is expired", 7 * 24 * 3600, types.int, true)
-    .addParam("bond", "Minimum bond that is required for an answer to be accepted", "0", types.string, true)
-    .setAction(async (taskArgs, hardhatRuntime) => {
-        const [caller] = await hardhatRuntime.ethers.getSigners();
-        console.log("Using the account:", caller.address);
-        const Module = await hardhatRuntime.ethers.getContractFactory("DaoModule");
-        const module = await Module.deploy(taskArgs.dao, taskArgs.oracle, taskArgs.timeout, taskArgs.cooldown, taskArgs.expiration, taskArgs.bond, taskArgs.template);
-
-        console.log("Module deployed to:", module.address);
-    });
-
-task("factory-setup", "Deploy and initialize DAO Module through a Proxy Factory")
+        )
+        .addParam("timeout", "Timeout in seconds that should be required for the oracle", 48 * 3600, types.int, true)
+        .addParam("cooldown", "Cooldown in seconds that should be required after a oracle provided answer", 24 * 3600, types.int, true)
+        .addParam("expiration", "Time duration in seconds for which a positive answer is valid. After this time the answer is expired", 7 * 24 * 3600, types.int, true)
+        .addParam("bond", "Minimum bond that is required for an answer to be accepted", "0", types.string, true)
+        .setAction(async (taskArgs, hardhatRuntime) => {
+            const [caller] = await hardhatRuntime.ethers.getSigners();
+            console.log("Using the account:", caller.address);
+            const Module = await hardhatRuntime.ethers.getContractFactory("DaoModule");
+            const module = await Module.deploy(taskArgs.owner, taskArgs.executor, taskArgs.oracle, taskArgs.timeout, taskArgs.cooldown, taskArgs.expiration, taskArgs.bond, taskArgs.template);
+            
+            console.log("Module deployed to:", module.address);
+        });
+        
+task("factorySetup", "Deploy and initialize DAO Module through a Proxy Factory")
     .addParam("factory", "Address of the Proxy Factory", undefined, types.string)
     .addParam("mastercopy", "Address of the DAO Module Master Copy", undefined, types.string)
-    .addParam("dao", "Address of the DAO (e.g. Safe)", undefined, types.string)
+    .addParam("owner", "Address of the owner", undefined, types.string)
+    .addParam("executor", "Address of the executor (e.g. Safe)", undefined, types.string)
     .addParam("oracle", "Address of the oracle (e.g. Realitio)", undefined, types.string)
     .addParam(
         "template", 
@@ -59,7 +61,8 @@ task("factory-setup", "Deploy and initialize DAO Module through a Proxy Factory"
         const Module = await hardhatRuntime.ethers.getContractFactory("DaoModule");
 
         const initParams = Module.interface.encodeFunctionData('setUp', [
-            taskArgs.dao, 
+            taskArgs.owner,
+            taskArgs.executor,
             taskArgs.oracle,
             taskArgs.timeout,
             taskArgs.cooldown,
@@ -74,7 +77,8 @@ task("factory-setup", "Deploy and initialize DAO Module through a Proxy Factory"
     });
 task("verifyEtherscan", "Verifies the contract on etherscan")
     .addParam("module", "Address of the module", undefined, types.string)
-    .addParam("dao", "Address of the DAO (e.g. Safe)", undefined, types.string)
+    .addParam("owner", "Address of the owner", undefined, types.string)
+    .addParam("executor", "Address of the executor (e.g. Safe)", undefined, types.string)
     .addParam("oracle", "Address of the oracle (e.g. Realitio)", undefined, types.string)
     .addParam(
         "template", 
@@ -90,7 +94,7 @@ task("verifyEtherscan", "Verifies the contract on etherscan")
         await hardhatRuntime.run("verify", {
             address: taskArgs.module,
             constructorArgsParams: [
-                taskArgs.dao, taskArgs.oracle, `${taskArgs.timeout}`, `${taskArgs.cooldown}`, `${taskArgs.expiration}`, `${taskArgs.bond}`, taskArgs.template
+                taskArgs.owner, taskArgs.executor, taskArgs.oracle, `${taskArgs.timeout}`, `${taskArgs.cooldown}`, `${taskArgs.expiration}`, `${taskArgs.bond}`, taskArgs.template
             ]
         })
     });
@@ -118,14 +122,14 @@ task("deployMasterCopy", "deploy a master copy of DAO Module").setAction(
         const [caller] = await hardhatRuntime.ethers.getSigners();
         console.log("Using the account:", caller.address);
         const Module = await hardhatRuntime.ethers.getContractFactory("DaoModule");
-        const module = await Module.deploy(FIRST_ADDRESS, ZERO_ADDRESS, 1, 0, 60, 0, 0);
+        const module = await Module.deploy(FIRST_ADDRESS, FIRST_ADDRESS, ZERO_ADDRESS, 1, 0, 60, 0, 0);
 
         await module.deployTransaction.wait(3);
 
         console.log("Module deployed to:", module.address);
         await hardhatRuntime.run("verify:verify", {
             address: module.address,
-            constructorArguments: [FIRST_ADDRESS, ZERO_ADDRESS, 1, 0, 60, 0, 0],
+            constructorArguments: [FIRST_ADDRESS, FIRST_ADDRESS, ZERO_ADDRESS, 1, 0, 60, 0, 0],
         });
     }
 );
