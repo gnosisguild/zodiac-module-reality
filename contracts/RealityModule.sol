67a45e44f48be022dc638b4547d7ee4606e65edd
cbrzn <cesarbrazon10@gmail.com>
2021-09-04 21:06:58 +0200

    chore: updated zodiac package name

--- contracts/RealityModule.sol
+++ contracts/RealityModule.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity >=0.8.0;
 
-import "@gnosis/zodiac/contracts/core/Module.sol";
+import "@zodiacdao/zodiac/contracts/core/Module.sol";
 import "./interfaces/RealitioV3.sol";
 
 abstract contract RealityModule is Module {
--- contracts/test/TestFactory.sol
+++ contracts/test/TestFactory.sol
@@ -1,4 +1,4 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity >=0.8.0;
 
-import "@gnosis/zodiac/contracts/factory/ModuleProxyFactory.sol";
+import "@zodiacdao/zodiac/contracts/factory/ModuleProxyFactory.sol";
