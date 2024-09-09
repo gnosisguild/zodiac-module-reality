387d07649d75fe10ff0493a90bb1d3426a9a9e14
juliopavila <javila1997@gmail.com>
2024-09-09 17:10:55 -0300

    chore: clean de code

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,59 +1,65 @@
-import "@nomicfoundation/hardhat-toolbox";
-import "@nomicfoundation/hardhat-verify";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
-import dotenv from "dotenv";
-import type { HttpNetworkUserConfig } from "hardhat/types";
-import yargs from "yargs";
-import "hardhat-contract-sizer";
-
-
-
+import '@nomicfoundation/hardhat-toolbox'
+import '@nomicfoundation/hardhat-verify'
+import 'hardhat-gas-reporter'
+import 'solidity-coverage'
+import dotenv from 'dotenv'
+import type { HttpNetworkUserConfig } from 'hardhat/types'
+import yargs from 'yargs'
+import 'hardhat-contract-sizer'
 
 const argv = yargs
-  .option("network", {
-    type: "string",
-    default: "hardhat",
+  .option('network', {
+    type: 'string',
+    default: 'hardhat',
   })
   .help(false)
-  .version(false).argv;
+  .version(false).argv
 
 // Load environment variables.
-dotenv.config();
-const { INFURA_KEY, MNEMONIC, ETHERSCAN_API_KEY, PK, ALCHEMY_KEY } =
-  process.env;
-  
-//   import "./src/tasks/deployDeterministicDeploymentHelper";
-// import "./src/tasks/setup";
-// import "./src/tasks/proposals";
+dotenv.config()
+const { INFURA_KEY, MNEMONIC, ETHERSCAN_API_KEY, PK, ALCHEMY_KEY } = process.env
+
+import "./src/tasks/extract-mastercopy";
+import "./src/tasks/deploy-mastercopies";
+import "./src/tasks/deploy-mastercopy";
+import "./src/tasks/verify-mastercopies";
+import "./src/tasks/verify-mastercopy";
+
 
 const DEFAULT_MNEMONIC =
-  "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+  'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat'
 
-const sharedNetworkConfig: HttpNetworkUserConfig = {};
+const sharedNetworkConfig: HttpNetworkUserConfig = {}
 if (PK) {
-  sharedNetworkConfig.accounts = [PK];
+  sharedNetworkConfig.accounts = [PK]
 } else {
   sharedNetworkConfig.accounts = {
     mnemonic: MNEMONIC || DEFAULT_MNEMONIC,
-  };
+  }
 }
 
-if (["mainnet", "sepolia"].includes(argv.network) && INFURA_KEY === undefined) {
+if (['mainnet', 'sepolia'].includes(argv.network) && INFURA_KEY === undefined) {
   throw new Error(
-    `Could not find Infura key in env, unable to connect to network ${argv.network}`
-  );
+    `Could not find Infura key in env, unable to connect to network ${argv.network}`,
+  )
 }
 
 export default {
   paths: {
-    artifacts: "build/artifacts",
-    cache: "build/cache",
-    deploy: "src/deploy",
-    sources: "contracts",
+    artifacts: 'build/artifacts',
+    cache: 'build/cache',
+    deploy: 'src/deploy',
+    sources: 'contracts',
   },
   solidity: {
-    compilers: [{ version: "0.8.0" }, { version: "0.6.12" }],
+    compilers: [
+      { version: '0.8.20' },
+      { version: '0.8.4' }, 
+      { version: '0.8.2' },
+      { version: '0.8.1' },
+      { version: '0.8.0' },
+      { version: '0.6.12' },
+    ],
   },
   networks: {
     mainnet: {
@@ -70,18 +76,18 @@ export default {
     },
     xdai: {
       ...sharedNetworkConfig,
-      url: "https://xdai.poanetwork.dev",
+      url: 'https://xdai.poanetwork.dev',
     },
     matic: {
       ...sharedNetworkConfig,
-      url: "https://polygon-rpc.com",
+      url: 'https://polygon-rpc.com',
     },
     bsc: {
       ...sharedNetworkConfig,
-      url: "https://bsc-dataseed.binance.org",
+      url: 'https://bsc-dataseed.binance.org',
     },
-    "truffle-dashboard": {
-      url: "http://localhost:24012/rpc",
+    'truffle-dashboard': {
+      url: 'http://localhost:24012/rpc',
       timeout: 100000000,
     },
   },
@@ -94,4 +100,4 @@ export default {
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
   },
-};
+}
